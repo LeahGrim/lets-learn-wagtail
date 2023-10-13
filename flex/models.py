55064dc0fa6106bdac8ae0093a1efd877d6219d6
Leah Grim <grimchelate@gmail.com>
2023-10-13 15:05:06 -0500

    create flex page.html

--- flex/models.py
+++ flex/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from wagtail.core.models import Page
+from wagtail.admin.edit_handlers import FieldPanel
 
-# Create your models here.
+class FlexPage(Page):
+    """Flexible Page Class. """
+
+    template = "flex/flex_page.html"
+
+    # @todo add streamfields
+    # content = StreamField()
+
+    subtitle = models.CharField(max_length=100, null=True, blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("subtitle"),
+    ]
+
+    class Meta: #noqa
+        verbose_name = "Flex Page"
+        verbose_name_plural = "Flex Pages"
