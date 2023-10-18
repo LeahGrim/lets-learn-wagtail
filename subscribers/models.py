c758ff1ee5fc5c337940da2b1c4f2a6069d732ff
Leah Grim <grimchelate@gmail.com>
2023-10-18 16:51:09 -0500

    created Subscribers model

--- subscribers/models.py
+++ subscribers/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Subscribers(models.Model):
+    """A subscribers model."""
+    email= models.CharField(blank=False, null=False, max_length=100, help_text="email address")
+    full_name= models.CharField(blank=False, null=False, max_length=100, help_text="First and Last name")
+
+    def __str__(self):
+        """string representation of Subscribers object"""
+        return self.full_name
