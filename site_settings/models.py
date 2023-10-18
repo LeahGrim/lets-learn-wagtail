bf02f09c8bd7076718a9d5ec7828b33514bda5f2
Leah Grim <grimchelate@gmail.com>
2023-10-18 16:10:05 -0500

    created models.py page for social media settings

--- site_settings/models.py
+++ site_settings/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel
+from wagtail.contrib.settings.models import BaseSetting, register_setting
 
-# Create your models here.
+@register_setting
+class SocialMediaSettings(BaseSetting):
+    """Social Media settings for our custom website """
+    
+    facebook = models.URLfield(blank=True, null=True, help_text="Facebook URL")
+    twitter = models.Charfield(blank=True, null=True, help_text="Twitter URL ")
+    youtube = models.Charfield(blank=True, null=True, help_text="Youtube Channel")
+    
+    panels = [
+        MultiFieldPanel([
+            FieldPanel("facebook"),
+            FieldPanel("twitter"), 
+            FieldPanel("youtube"),
+        ], heading="Social Media Setting")
+    ]
