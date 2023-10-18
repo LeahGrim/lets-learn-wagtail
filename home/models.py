31f0515c24cada450da46231de64e8010690e4fb
Leah Grim <grimchelate@gmail.com>
2023-10-18 15:00:54 -0500

    created a carousel image chooser for banner on homepage

--- home/models.py
+++ home/models.py
@@ -1,10 +1,25 @@
 from django.db import models
 
-from wagtail.core.models import Page
+from modelcluster.fields import ParentalKey
+from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField, StreamField 
 from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel, StreamFieldPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 from streams import blocks
+
+class HomepageCarouselImages(Orderable):
+    """Between 1-5 images for the home page carousel."""
+    page = ParentalKey("home.HomePage", related_name= "carousel_images")
+    carousel_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True, 
+        blank=False, 
+        on_delete=models.SET_NULL, 
+        related_name="+",
+    )
+    panels = [
+        ImageChooserPanel("carousel_image")
+    ]
 
 class HomePage(Page):
     """Home page model."""
