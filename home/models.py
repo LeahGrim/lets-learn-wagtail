d26762dc585dbf9da0ba194865eb82e6106c7ef6
Leah Grim <grimchelate@gmail.com>
2023-10-18 14:51:53 -0500

    adding a call to action component

--- home/models.py
+++ home/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 from wagtail.core.models import Page
-from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel
+from wagtail.core.fields import RichTextField, StreamField 
+from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel, StreamFieldPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
-
+from streams import blocks
 
 class HomePage(Page):
     """Home page model."""
@@ -29,11 +29,22 @@ class HomePage(Page):
         related_name="+",
     )
 
+    content = StreamField(
+        [
+            ("cards", blocks.SimpleRichtextBlock()),
+            ("cta", blocks.CTABlock()),
+
+        ],
+        null=True, 
+        blank=True,
+    ) 
+
     content_panels = Page.content_panels + [
         FieldPanel("banner_title"),
         FieldPanel("banner_subtitle"),
         ImageChooserPanel("banner_image"),
-        PageChooserPanel("banner_cta")
+        PageChooserPanel("banner_cta"),
+        StreamFieldPanel("content"),
     ]
 
     class Meta:
