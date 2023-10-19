c1177aa2f1d4ce07625af67cc047fcac0db94602
Leah Grim <grimchelate@gmail.com>
2023-10-19 14:45:24 -0500

    added title textField to subscribers model

--- subscribers/models.py
+++ subscribers/models.py
@@ -5,6 +5,7 @@ class Subscribers(models.Model):
     """A subscribers model."""
     email= models.CharField(blank=False, null=False, max_length=100, help_text="email address")
     full_name= models.CharField(blank=False, null=False, max_length=100, help_text="First and Last name")
+    title= models.TextField(blank=True, null=True, max_length=50, help_text="Job Title, if desired")
 
     def __str__(self):
         """string representation of Subscribers object"""
