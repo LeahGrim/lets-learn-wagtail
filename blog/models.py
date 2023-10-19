7d2b87053d87c08f7541ac31f5f7fdecb02744d3
Leah Grim <grimchelate@gmail.com>
2023-10-19 14:57:12 -0500

    created BlogListingPage class

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
-from django.db import models
 
+from django.db import models
+from wagtail.core.models import Page
 # Create your models here.
+
+class BlogListingPage(Page):
+    """Listing Page lists all the blog detail pages."""
+    custom_title = models.CharField(
+        max_length=200, 
+        blank=False, 
+        null=False, 
+        help_text="overwrite default title",
+        )
+    def get_context(self, request, *args, **kwargs):
+        """adding custom stuff to our context."""
+        context = super().get_context(request,*args, **kwargs)
+        #context['extra'] = 'extra things to display as string in admin page" 
+        return context
+# class BlogDetailPage(Page):
