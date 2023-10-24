865222d123f51387a227bd19b6df6489201f8dd7
Leah Grim <grimchelate@gmail.com>
2023-10-24 11:41:58 -0500

    added Resources page into appropriate models.py

--- resources/models.py
+++ resources/models.py
@@ -1,3 +1,25 @@
+"""Resources Page"""
+
 from django.db import models
+from wagtail.core.models import Page
+from wagtail.admin.edit_handlers import FieldPanel
 
 # Create your models here.
+class ResourcePage(Page):
+    """Resources Page Class"""
+
+    template = "resources/resources_page.html"
+
+    #todo add streamfields 
+    # content= StreamField
+
+    subtitle = models.CharField(max_length=100, null=True, blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("subtitle"),
+
+    ]
+
+    class Meta: #noqa
+        verbose_name = "Resources Page"
+        verbose_name_plural = "Resources Pages"
